4b793ae21414a53c3bbc5f8a0a8f130684d75973
pivotdude <egor.corostin@gmail.com>
2024-05-19 13:42:52 +0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Sidebar } from './components/sidebar/Sidebar'
+import { Button } from './components/ui/Button'
+
+function App() {
+  return (
+    <>
+      <Sidebar />
+    </>
+  )
+}
+
+export default App
