ae935ec99f0e3458bd8c6033519d8859c21a314c
pivotdude <egor.corostin@gmail.com>
2024-06-07 21:11:03 +0400

    init

---
+++ src/app/router/appRouter.tsx
@@ -0,0 +1,14 @@
+import {createBrowserRouter} from "react-router-dom";
+
+export function AppRouter() {
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <div>Hello world!</div>,
+        },
+    ]);
+
+    return (
+
+    )
+}
--- src/widgets/sidebar/ui/sidebar.tsx
+++ src/widgets/sidebar/ui/sidebar.tsx
@@ -1,6 +1,6 @@
 "use server"
+
 export function Sidebar() {
-  console.log('sidebar')
     return (
         <div className="fixed h-full bg-sky-200 space-x-2 w-16 flex justify-center">
             <div className="h-8 w-8 cursor-pointer flex jus">
